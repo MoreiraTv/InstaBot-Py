0f403adac9a9caf748112e1a314950548a243838
willkj <wdwill55@gmail.com>
2022-01-20 11:22:51 -0300

    token adicionado

--- Will/boot.py
+++ Will/boot.py
@@ -5,12 +5,13 @@ import random
 
 
 class InstagramBot:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.driver = webdriver.Firefox(
-            executable_path=r"./geckodriver.exe"
+    def __init__(self, cookies):
+        self.cookies = cookies
+        self.driver = webdriver.Chrome(
+            executable_path=r"./chromedriver.exe"
         )  # Coloque o caminho para o seu geckodriver aqui
+        # executable_path=r"./geckodriver.exe"
+
 
     def login(self):
         driver = self.driver
@@ -24,24 +25,41 @@ class InstagramBot:
         except:
             print('já estamos na página de login')
             pass
-        user_element = driver.find_element_by_xpath(
-            "//input[@name='username']")
-        user_element.clear()
-        time.sleep(random.randint(4, 6))
-        user_element.send_keys(self.username)
-        time.sleep(random.randint(4, 6))
-        password_element = driver.find_element_by_xpath(
-            "//input[@name='password']")
-        password_element.clear()
-        password_element.send_keys(self.password)
-        time.sleep(random.randint(4, 6))
-        password_element.send_keys(Keys.RETURN)
-        time.sleep(random.randint(4, 6))
+        
+        time.sleep(3)
+        #entrando na conta de usario pela sesion Id token sem precisar da senha do usuario
+        driver.add_cookie(self.cookies)
+
+        print("entrando na conta de usario pela sesion Id token sem precisar da senha do usuario")
+        driver.refresh()
+
+        time.sleep(3)
+
+        sizeElement = len(driver.find_elements_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]'))
+        isPresent = sizeElement > 0
+        
+        if isPresent:
+            driver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]').click()
+        
+        #/html/body/div[5]/div/div/div/div[3]/button[2]
+        
+        self.capturaFollowCount()
+        
+    
+    def capturaFollowCount(self):
+        driver = self.driver
+        driver.find_element_by_xpath('//section/nav/div[2]/div/div/div[3]/div/div[6]/span').click()
+        time.sleep(3)
+        driver.find_element_by_xpath('//section/nav/div[2]/div/div/div[3]/div/div[6]/div[2]/div[2]/div[2]/a[1]').click()
+        time.sleep(2)
+        follows = driver.find_element_by_xpath('//header/section/ul/li[2]/a/span').text
+        
+        print(follows)
+        #//header/section/ul/li[1]/a/span
+
         self.curtir_fotos_com_a_hastag(
-            "programação"
+            "programaçao"
         )  # Altere aqui para a hashtag que você deseja usar.
-
-
 
     @staticmethod
     def type_like_a_person(sentence, single_input_field):
@@ -81,40 +99,25 @@ class InstagramBot:
                 "window.scrollTo(0, document.body.scrollHeight);")
             try:
                 driver.find_element_by_xpath(
-                    '//button[@type="button"]').click()
-                driver.find_element_by_xpath(
                     '//section/span/button[@type="button"]').click()
                 time.sleep(random.randint(19, 23))
             except Exception as e:
                 print(e)
                 time.sleep(5)
-                
-        self.seguirFollowPerfil("filipedeschamps", 50)
-
-    def capturaFollowCount(self):
-        driver = self.driver
-        driver.find_element_by_xpath('//section/nav/div[2]/div/div/div[3]/div/div[6]/span').click()
-        time.sleep(3)
-        driver.find_element_by_xpath('//section/nav/div[2]/div/div/div[3]/div/div[6]/div[2]/div[2]/div[2]/a[1]').click()
-        time.sleep(2)
-        follows = driver.find_element_by_xpath('//header/section/ul/li[2]/a/span').text
         
-        print(follows)
-        #//header/section/ul/li[1]/a/span
-
-        self.curtir_fotos_com_a_hastag(
-            "programaçao"
-        )  # Altere aqui para a hashtag que você deseja usar.
-
+        self.seguirFollowPerfil("filipedeschamps", 50)
+    
     def seguirFollowPerfil(self, perfil, countFollow):
         driver = self.driver
 
         driver.get("https://www.instagram.com/" + perfil)
         time.sleep(3)
         hrefPerfil = '/'+ perfil + '/followers/'
-        driver.find_element(By.HREF, hrefPerfil ).click
-        driver.find_element_by_xpath('/html/body/div[6]/div/div/div[2]/ul/div')
-        time.sleep(3)
+        elems = driver.find_element_by_css_selector("#react-root > section > main > div > header > section [href]").click()
+        
+        time.sleep(2)
+        driver.find_element_by_css_selector('body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div')
+        time.sleep(2)
         rounds = (countFollow / 12).ceil()
         print(rounds)
         for i in range(
@@ -124,8 +127,16 @@ class InstagramBot:
                 "window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(3)
 
+with open("../token_value", "r", encoding="utf-8") as tf:
+    cookie_value = tf.read()
+
+with open("../token_expiration", "r", encoding="utf-8") as tf:
+    cookie_expiration = tf.read()
 
 jhonatanBot = InstagramBot(
-    "botzaopog", "ocarahackeouobot"
+    #está com 170 follows
+    # {'domain': '.instagram.com', 'expiry': 1674084111, 'httpOnly': True, 'name': 'sessionid', 'path': '/', 'secure': True, 'value': '51284129573%3AVzR0jTdM6aRo94%3A0'}
+    # {'domain': '.instagram.com', 'expiry': 1674219274, 'httpOnly': True, 'name': 'sessionid', 'path': '/', 'secure': True, 'value': '51284129573%3Ag1z6etzQzy2Spv%3A2'}
+    { "domain": ".instagram.com", "expirationDate": cookie_expiration, "httpOnly": True, "name": "sessionid", "path": "/", "secure": True, "session": False, "value": cookie_value},
 )  # Entre com o usuário e senha aqui
 jhonatanBot.login()
