a25a31e2cd55c8ab1f0548a05d8983c5fcc0aa36
Matheus Moreira <matheusmoreira08@gmail.com>
2022-01-21 11:09:32 -0300

    att codigos para rollar page follow

--- Will/seguir.py
+++ Will/seguir.py
@@ -1,3 +1,4 @@
+import math
 def seguirFollowPerfil(self, perfil, countFollow):
     driver = self.driver
 
@@ -9,11 +10,12 @@ def seguirFollowPerfil(self, perfil, countFollow):
     time.sleep(2)
     driver.find_element_by_css_selector('body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div')
     time.sleep(2)
-    rounds = (countFollow / 12).ceil()
+    rounds = math.ceil((countFollow / 12))
     print(rounds)
     for i in range(
         1, rounds
     ):  # Altere o segundo valor aqui para que ele desça a quantidade de páginas que você quiser: quer que ele desça 5 páginas então você deve alterar de range(1,3) para range(1,5)
         driver.execute_script(
-            "window.scrollTo(0, document.body.scrollHeight);")
+            "let divElem = document.querySelector('body > div.RnEpo.Yx5HN > div > div > div.isgrP');document.querySelector('body > div.RnEpo.Yx5HN > div > div > div.isgrP').scrollTop +=357;"
+            )
         time.sleep(3)
